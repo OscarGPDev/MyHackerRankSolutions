36fbc5a4e2fcabc8ecd4f52bbec54ef37a98e550
Oscar G. Perez <oscarprograb@gmail.com>
2025-03-04 07:37:53 -0600

    identation for twoStrings

--- twoStrings.js
+++ twoStrings.js
@@ -15,10 +15,10 @@ function twoStrings(s1, s2) {
     They say a minimun substring could be a single character, and they just ask if they share any substring.
     Looking fo the largest substring or something it's not required and due to effort and computing cost I'm not doing it.
     */
-   for (const character of s1) {
-    if(s2.includes(character)){
-        return "YES"
+    for (const character of s1) {
+        if (s2.includes(character)) {
+            return "YES"
+        }
     }
-   }
-   return "NO"
+    return "NO"
 }
