6a262a904ae53c9db9458324b009eb637f3a9b6f
Oscar G. Perez <oscarprograb@gmail.com>
2025-05-10 07:23:58 -0600

    catAndMouse solution

---
+++ catAndMouse.js
@@ -0,0 +1,16 @@
+// Complete the catAndMouse function below.
+function catAndMouse(x, y, z) {
+    const zxAbsDiff = Math.abs(z - x)
+    const zyAbsDiff = Math.abs(z - y)
+    let result
+    if (zxAbsDiff === zyAbsDiff) {
+        result = "Mouse C"
+    } else if (zxAbsDiff < zyAbsDiff) {
+        result = "Cat A"
+    } else if (zyAbsDiff < zxAbsDiff) {
+        result = "Cat B"
+    }
+
+    return result
+}
+console.log(catAndMouse(2, 5, 4))
