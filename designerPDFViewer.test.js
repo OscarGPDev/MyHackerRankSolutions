494d14f234e92628d76c05f276078b39db08667d
Oscar G. Perez <oscarprograb@gmail.com>
2025-05-17 11:23:16 -0600

    so... I'm going into test driven development(?

---
+++ designerPDFViewer.test.js
@@ -0,0 +1,16 @@
+const designerPdfViewer = require('./designerPdfViewer');
+
+test('designerPdfViewer (designerPdfViewer(h, "torn")', () => {
+    expect(designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 1, 1, 5, 5, 1, 5, 2, 5, 5, 5, 5, 5, 5],
+        "torn")).toBe(8);
+});
+
+test('designerPdfViewer (designerPdfViewer(h, "abc")', () => {
+    expect(designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+        "abc")).toBe(9);
+});
+
+test('designerPdfViewer (designerPdfViewer(h, "zaba")', () => {
+    expect(designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7],
+        "zaba")).toBe(28);
+});
