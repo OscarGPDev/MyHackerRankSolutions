440e3e38d3c65acded44cff26160065b3873a2c4
Oscar G. Perez <oscarprograb@gmail.com>
2025-05-24 08:50:33 -0600

    pickingNumbers solution, first I didn't notice this two subarrays aproach but I think this way is cleanner

--- pickingNumbers.js
+++ pickingNumbers.js
@@ -1,4 +1,32 @@
-function pickingNumbers(a){
+function pickingNumbers(a) {
+    let result = 0
+    const calculatedSet = new Set()
+    a.forEach((element, index) => {
+        let elementUpperSubarrayLength = 0
+        let elementLowerSubarrayLength = 0
+        let higher = element + 1
+        let lower = element - 1
+        if (calculatedSet.has(element)) {
+            return
+        } else {
+            calculatedSet.add(element)
+        }
 
+        for (let subIndex = index; subIndex < a.length; subIndex++) {
+            const subElement = a[subIndex];
+
+            if (subElement === element || subElement === higher) {
+                elementUpperSubarrayLength++
+            }
+            if (subElement === element || subElement === lower) {
+                elementLowerSubarrayLength++
+            }
+        }
+        const largerSubArray = elementLowerSubarrayLength >= elementUpperSubarrayLength ? elementLowerSubarrayLength : elementUpperSubarrayLength
+        if (largerSubArray > result) {
+            result = largerSubArray
+        }
+    });
+    return result
 }
-module.exports=pickingNumbers
+module.exports = pickingNumbers
